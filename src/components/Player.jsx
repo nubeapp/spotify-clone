fe934408a1d134e785ec6a467187f8cc1bc99703
gitalvarolopez <lopezsierraalvaro@gmail.com>
2023-11-14 20:09:08 +0100

    Initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,54 @@
+import { usePlayerStore } from "@/store/playerStore"
+import { useState, useRef, useEffect } from "react"
+
+export const Pause = ({ className }) => (
+    <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
+)
+  
+export const Play = ({ className }) => (
+    <svg className={className} role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
+)
+
+export function Player() {
+    const { isPlaying, setIsPlaying } = usePlayerStore(state => state)
+    const [currentSong, setCurrentSong] = useState(null)
+    const audioRef = useRef(null)
+
+    useEffect(() => {
+        audioRef.current.src = `/music/1/01.mp3`
+    }, [])
+
+    const handleClick = () => {
+        if(isPlaying) {
+            audioRef.current.pause()
+        } else {
+            audioRef.current.play()
+            // audioRef.current.volume = 0.1
+        }
+
+        setIsPlaying(!isPlaying)
+    }
+
+    return (
+        <div className="flex flex-row justify-between w-full px-4 z-50">
+            <div className="">
+                Current song...
+            </div>
+
+            <div className="grid place-content-center gap-4 flex-1">
+                <div className="flex justify-center"></div>
+                <button className="bg-white rounded-full p-2" onClick={handleClick}>
+                    {isPlaying ? <Pause className="w-4 h-4" /> : <Play className="w-4 h-4" />}
+                </button>
+            </div>
+
+            <div className="grid place-content-center">
+                Volumen
+            </div>
+
+            <audio ref={audioRef}></audio>
+        </div>
+    )
+}
+
+  
